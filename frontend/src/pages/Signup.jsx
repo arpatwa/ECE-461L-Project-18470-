c6acec668b4503b3eb3b054415e09dc994c10383
Illango Zaruba <90343447+TimillaO3O@users.noreply.github.com>
2026-02-23 13:08:16 -0600

    New branch

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,3 +1,3 @@
 export default function Signup() {
-    return <div style={{ padding: 40 }}>Signup page (placeholder)</div>;
+    return <div style={{ padding: 40 }}>Signup page (placeholder)1</div>;
 }

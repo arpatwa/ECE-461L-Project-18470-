7131bbcbce377b66b43a0372c972ea8d2da32771
Illango Zaruba <90343447+TimillaO3O@users.noreply.github.com>
2026-03-30 14:58:22 -0500

    Frontend Backend implementation and some fixes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,17 +4,20 @@ import Login from "./pages/Login.jsx";
 import Signup from "./pages/Signup.jsx";
 import HardwareUI from "./pages/HardwareUI.jsx";
 import "./App.css"; 
+import './index.css';
 
 export default function App() {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Homepage />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/signup" element={<Signup />} />
-                <Route path="/hardware" element={<HardwareUI />} />
-                <Route path="*" element={<Navigate to="/" replace />} />
-            </Routes>
-        </BrowserRouter>
+        <div className="App">
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Homepage />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/signup" element={<Signup />} />
+                    <Route path="/hardware" element={<HardwareUI />} />
+                    <Route path="*" element={<Navigate to="/" replace />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
     );
 }
